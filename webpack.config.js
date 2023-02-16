1b6a629ee545c040f3fa2226953ecfb036529b97
Odongo006 <www.henryodongo006@gmail.com>
2023-02-16 23:42:56 +0300

    change plugin

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
+      template: 'src/index.html',
+      filename: 'index.html',
     }),
   ],
   output: {

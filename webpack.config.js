5e11df6f5990bf91090e391c83cfea7cd7beabe7
Odongo006 <www.henryodongo006@gmail.com>
2023-02-17 15:29:18 +0300

    Do some installation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -14,6 +15,11 @@ module.exports = {
     port: 8080,
     hot: false,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
